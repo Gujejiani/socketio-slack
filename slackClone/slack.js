4d0891c81f358e61feb8be35e8acd2b68d67bd79
kakha <kakha.gujejiani.1@iliauni.edu.ge>
2023-07-24 12:52:20 +0400

    vizual update

---
+++ slackClone/slack.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const socketio = require('socket.io');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+
+app.use(express.static(__dirname + '/public'));
+
+const expressServer = app.listen(9000);
+
+    const io = socketio(expressServer)
+
+    io.on('connection', (socket) => {
+        console.log(socket.id, 'connected');
+        // in ws we use send method and in socket.io we use emit event
+    //socket.emit('messageFromServer', { data: 'Welcome to the socketio server' });
+
+        socket.emit('welcome to the server baby')
+})
+
+
+
+
+
+
